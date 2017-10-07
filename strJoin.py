405ebd0e433a87eb6af483eac0a47cded251fb1c
zhglinux <zhglinux@126.com>
2017-10-07 21:07:24 +0800

    add code of str connect

---
+++ strJoin.py
@@ -0,0 +1,25 @@
+
+# -*- coding: utf-8 -*-
+"""
+Mac OS :OS X EI Captian
+python verion: 2.7
+Editor:zhglinux
+This is a test script file.
+"""
+
+
+
+strT1 = 'this ' + 'is ' + 'test ' + "string."
+print  strT1
+
+
+strT2 = ['this ', 'is ', 'test ', 'string. ']
+print  ''.join(strT2)
+
+
+strT3  = '%s%s%s%s' % ('this ', 'is ', 'test ', 'string. ')
+print  strT3
+
+
+
+
